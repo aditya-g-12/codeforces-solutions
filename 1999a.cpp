68e4a50c10049ae1c257baf6cde0728acca1a12a
Adi <adityaarushi2@gmail.com>
2026-06-13 00:32:23 +0530

    1999a

---
+++ 1999a.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a;
+    for (int i=0;i<n;i++){
+        cin>>a;
+        cout<<((a%10)+((a-a%10)/10))<<endl;
+    }
+}
