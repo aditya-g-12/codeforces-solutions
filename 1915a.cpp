7520974b05be5ee3e5396804f70e4a50ec2b15a7
Adi <adityaarushi2@gmail.com>
2026-06-15 01:34:25 +0530

    1915a

---
+++ 1915a.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a,b,c;
+    for(int i=0;i<n;i++){
+        cin>>a>>b>>c;
+        if(a==b){
+            cout<<c<<endl;
+        }
+        else{ if(a==c){
+            cout<<b<<endl;
+        }
+        else{
+            cout<<a<<endl;
+        }
+        }
+    }
+}
