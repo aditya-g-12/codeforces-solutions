d70554763062fcaee04052523279310d5585300d
Adi <adityaarushi2@gmail.com>
2026-06-22 23:02:44 +0530

    1624a

---
+++ 1624a.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int a;
+        cin>>a;
+        int arr[a];
+        for(int i=0;i<a;i++){
+            cin>>arr[i];
+        }
+        sort(arr,arr+a);
+        cout<<(arr[a-1]-arr[0])<<endl;
+    }
+}
