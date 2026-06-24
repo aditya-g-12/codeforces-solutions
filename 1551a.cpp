9f59296f7c9377320a3973a3a5800860bfe5fca3
Adi <adityaarushi2@gmail.com>
2026-06-24 22:27:47 +0530

    1551a

---
+++ 1551a.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int a;
+        cin>>a;
+        if(a%3==0){
+            cout<<(a/3)<<" "<<(a/3)<<endl;
+        }
+        else{
+            if(a%3==1){
+                cout<<(a/3)+1<<" "<<(a/3)<<endl;
+            }
+            else{
+                cout<<(a/3)<<" "<<(a/3)+1<<endl;
+            }
+            }
+        }
+    }
