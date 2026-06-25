5833b345498187f4c11130db052fdbcccdffa49b
Adi <adityaarushi2@gmail.com>
2026-06-25 22:28:52 +0530

    1985B

---
+++ 1985b.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int a;
+        cin>>a;
+        if(a==3){
+            cout<<"3"<<endl;
+        }
+        else{
+            cout<<"2"<<endl;
+        }
+    }
+}
