0c7fa2f0e4a93f60147aef54d91334e00137d9d7
Adi <adityaarushi2@gmail.com>
2026-07-10 22:30:55 +0530

    1660a

---
+++ 1660a.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        if(a==0){
+            cout<<1<<endl;
+        }
+        else{
+            cout<<(2*b)+a+1<<endl;
+        }
+    }
+}
