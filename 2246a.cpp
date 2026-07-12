6bb9918b7ceef17ebf044db39afb6f38a08f68fd
Adi <adityaarushi2@gmail.com>
2026-07-12 20:51:07 +0530

    2246a

---
+++ 2246a.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        while(n!=0){
+            cout<<n<<" ";
+            n--;
+
+        }
+        cout<<endl;
+    }
+}
